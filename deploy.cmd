29475b9a70728de3a393586a1ea2987083cc3e8f
Andrii Bida <ab.nomicos@gmail.com>
2020-05-29 22:37:21 +0200

    Add auto-deploy step to the script

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,6 @@
 xcopy /E /I /Y etc "%CATALINA_HOME%\webapps\HelloJSP\WEB-INF"
 xcopy /E /I /Y classes "%CATALINA_HOME%\webapps\HelloJSP\WEB-INF\classes"
 xcopy /E /I /Y web "%CATALINA_HOME%\webapps\HelloJSP"
+
+REM Touch the web.xml prompting Tomcat to auto-deploy the new version.
+type nul>>"%CATALINA_HOME%\webapps\HelloJSP\WEB-INF\web.xml"
